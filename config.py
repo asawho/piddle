e1699be348bc8a2c974f6eed41834877a7fc2591
asa <asa>
2019-11-15 22:34:21 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,105 @@
+import logging
+
+########################################################################
+#
+#   General options
+
+### Logging
+log_level = logging.INFO
+log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+
+### Server
+listening_ip = "0.0.0.0"
+listening_port = 8081
+
+### Cost Estimate
+kwh_rate        = 0.13  # Rate in currency_type to calculate cost to run job
+currency_type   = "$"   # Currency Symbol to show when calculating cost to run job
+
+########################################################################
+#
+#   GPIO Setup (BCM SoC Numbering Schema)
+#
+#   Check the RasPi docs to see where these GPIOs are
+#   connected on the P1 header for your board type/rev.
+#   These were tested on a Pi B Rev2 but of course you
+#   can use whichever GPIO you prefer/have available.
+
+### Outputs
+gpio_heat = 23  # Switches zero-cross solid-state-relay
+heater_invert = 0 # switches the polarity of the heater control
+
+### Thermocouple Adapter selection:
+#   max31855 - bitbang SPI interface
+#   max31855spi - kernel SPI interface
+#   max6675 - bitbang SPI interface
+max31855 = 0
+max6675 = 0
+max31855spi = 0 # if you use this one, you MUST reassign the default GPIO pins
+mcp9600 = 1
+
+### Thermocouple Connection (using bitbang interfaces)
+gpio_sensor_cs = 27
+gpio_sensor_clock = 22
+gpio_sensor_data = 17
+
+### Thermocouple SPI Connection (using adafrut drivers + kernel SPI interface)
+spi_sensor_chip_id = 0
+
+### duty cycle of the entire system in seconds. Every N seconds a decision
+### is made about switching the relay[s] on & off and for how long.
+### The thermocouple is read five times during this period and the highest
+### value is used.
+sensor_time_wait = 1
+
+
+########################################################################
+#
+#   PID parameters
+
+pid_kp = 0.05   #25  # Proportional
+pid_ki = 0.001  # Integration
+pid_kd = 0.0     # Derivative was 217
+
+
+########################################################################
+#
+#   Simulation parameters
+
+sim_t_env      = 25.0   # deg C
+sim_c_heat     = 100.0  # J/K  heat capacity of heat element
+sim_c_oven     = 5000.0 # J/K  heat capacity of oven
+sim_p_heat     = 5450.0 # W    heating power of oven
+sim_R_o_nocool = 1.0    # K/W  thermal resistance oven -> environment
+sim_R_o_cool   = 0.05   # K/W  " with cooling
+sim_R_ho_noair = 0.1    # K/W  thermal resistance heat element -> oven
+sim_R_ho_air   = 0.05   # K/W  " with internal air circulation
+
+
+########################################################################
+#
+#   Time and Temperature parameters
+
+temp_scale          = "f" # c = Celsius | f = Fahrenheit - Unit to display 
+time_scale_slope    = "h" # s = Seconds | m = Minutes | h = Hours - Slope displayed in temp_scale per time_scale_slope
+time_scale_profile  = "m" # s = Seconds | m = Minutes | h = Hours - Enter and view target time in time_scale_profile
+
+# emergency shutoff the kiln if this temp is reached.
+# when solid state relays fail, they usually fail closed.  this means your
+# kiln receives full power until your house burns down.
+# this should not replace you watching your kiln or use of a kiln-sitter
+emergency_shutoff_temp = 2250
+
+# not used yet
+# if measured value is N degrees below set point
+warning_temp_low = 5
+
+# not used yet
+# if measured value is N degrees above set point
+warning_temp_high = 5
+
+# thermocouple offset
+# If you put your thermocouple in ice water and it reads 36F, you can
+# set set this offset to -4 to compensate.  This probably means you have a
+# cheap thermocouple.  Invest in a better thermocouple.
+thermocouple_offset=0
---
+++ hardware-tests/mcp9600-test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import mcp9600
+import time
+
+m = mcp9600.MCP9600()
+
+print("Resetting alerts")
+for x in range(1, 5):
+    m.clear_alert(x)
+    m.configure_alert(x, enable=False)
+
+print("Configuring alerts")
+m.configure_alert(1, monitor_junction=0, limit=40, mode=1, enable=True)
+m.configure_alert(2, monitor_junction=0, limit=40, mode=1, enable=True, rise_fall=0)
+m.configure_alert(3, monitor_junction=0, limit=40, mode=1, enable=True, rise_fall=1)
+
+while True:
+    t = m.get_hot_junction_temperature()
+    c = m.get_cold_junction_temperature()
+    d = m.get_temperature_delta()
+
+    alerts = m.check_alerts()
+
+    for x in range(1, 5):
+        if alerts[x - 1] == 1:
+            m.clear_alert(x)
+
+    print("Alerts: ", alerts)
+
+    print("Hot: {}, Cold {}, Delta {}".format(t,c,d))
+
+    time.sleep(1.0)
+
